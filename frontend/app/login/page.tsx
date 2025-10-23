dbad10ce518e733fc05d82fac87bfb9c37d876ea
Umaya Hewaarachchi <umayaush@gmail.com>
2025-10-22 18:12:21 -0600

    Login Page basic styling

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -10,23 +10,31 @@ export default function LoginPage() {
 
 
     return (
-        <main className="flex flex-col items-center justify-center h-screen">
-            <div>
-                <h1>Login</h1>
-                <input
+        <div className="flex flex-col items-center justify-center h-screen w-screen bg-gray-100">
+            {/* Login Card */}
+            <div className="flex flex-col shadow-xl p-8 rounded-xl bg-white max-w-xs w-full sm:max-w-sm md:max-w-md">
+                <h1 className="self-center mb-5 font-bold text-2xl">Login</h1>
+                <label htmlFor="username">Username</label>
+                <input 
+                    className="mt-1 mb-4 p-1.5 border-2 rounded border-gray-300 "
                     type="text"
-                    placeholder="Username"
+                    id="username"
+                    placeholder="Enter Username"
                 />
-                <input
-                    type="text"
-                    placeholder="Password"
+                <label htmlFor="password">Password</label>
+                <input 
+                    className="mt-1 mb-4 p-1.5 border-2 rounded border-gray-300 "
+                    type="password"
+                    id="password"
+                    placeholder="Enter Password"
                 />
                 <button
+                    className="mt-4.5 p-2 rounded font-bold text-white bg-gray-600 hover:bg-gray-700 hover:cursor-pointer"
                     onClick={handleLogin}
                 >
                     Login
                 </button>
             </div>
-        </main>
+        </div>
 );
 }
